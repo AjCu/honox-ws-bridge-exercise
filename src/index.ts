5788bff549e262df9cbf6ef1dd2250ba28aad256
Alberto Cristancho <albertocristancho@outlook.com>
2024-06-13 22:59:10 -0500

    feat: add initial base to server ws

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { createBunWebSocket } from "hono/bun";
+
+const { upgradeWebSocket, websocket } = createBunWebSocket();
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get(
+  "/ws",
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send("Hello from server!");
+      },
+      onClose: () => {
+        console.log("Connection closed");
+      },
+    };
+  })
+);
+
+Bun.serve({
+  fetch: app.fetch,
+  websocket,
+});
+
+export default app;
