c576f72b9c1abe7e3a3c1a0de4b133339b5502ec
Alberto Cristancho <albertocristancho@outlook.com>
2024-06-14 23:24:55 -0500

    feat: handle communications properly

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,10 @@ const handleCarRequest = (event: MessageEvent, ws: WSContext) => {
   console.info("Car request received...");
   const newCar = JSON.parse(event.data.toString()) as Car;
   if (!isValidCar(newCar)) {
-    ws.send("Invalid car request");
+    const errorMessage = "Invalid car request";
+    const message = { error: errorMessage };
+    const messageString = JSON.stringify(message);
+    ws.send(messageString);
     console.error("Invalid car request");
     return;
   }
@@ -39,7 +42,9 @@ const handleCarRequest = (event: MessageEvent, ws: WSContext) => {
   setTimeout(() => {
     const index = carsArray.findIndex((car) => car.id === newCar.id);
     carsArray.splice(index, 1);
-    ws.send("Car with id " + newCar.id + " has passed the bridge");
+    const message = { id: newCar.id };
+    const messageString = JSON.stringify(message);
+    ws.send(messageString);
     console.info("Car with id " + newCar.id + " has passed the bridge");
   }, acumulatedTime * 1000);
 };
